f7eb86442c9cbe26ab9987ce53d87b870ca8ea16
BorisSonin <bsonin85@gmail.com>
2022-10-23 11:26:04 +0300

    The final version. Adding functionality for a button

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -8,20 +8,45 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet var redLightView: UIView!
     @IBOutlet var yellowLightView: UIView!
     @IBOutlet var greenLightView: UIView!
     
-    @IBOutlet var switchButton: UIView!
+    @IBOutlet var switchButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        redLightView.alpha = 0.5
+        yellowLightView.alpha = 0.5
+        greenLightView.alpha = 0.5
+        
+        
+        redLightView.layer.cornerRadius = 75
+        yellowLightView.layer.cornerRadius = 75
+        greenLightView.layer.cornerRadius = 75
+        
+        
     }
     @IBAction func switchButtonTapped() {
+        switchButton.setTitle("NEXT", for: .normal)
+        
+        if redLightView.alpha == 0.5 && yellowLightView.alpha == 0.5 && greenLightView.alpha == 0.5 {
+            redLightView.alpha = 1
+            
+        } else if redLightView.alpha == 1 && yellowLightView.alpha == 0.5 && greenLightView.alpha == 0.5 {
+            redLightView.alpha = 0.5
+            yellowLightView.alpha = 1
+            
+        } else if redLightView.alpha == 0.5 && yellowLightView.alpha == 1 && greenLightView.alpha == 0.5 {
+            yellowLightView.alpha = 0.5
+            greenLightView.alpha = 1
+            
+        } else if redLightView.alpha == 0.5 && yellowLightView.alpha == 0.5 && greenLightView.alpha == 1 {
+            greenLightView.alpha = 0.5
+            
+        }
+        
     }
-    
-
 }
-
